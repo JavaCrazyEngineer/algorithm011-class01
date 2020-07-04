94da135feacc8920286c0af6ce2bd1622b169c88
YCKJ0837 <mingxiufeng@cloudwalk.cn>
2020-07-05 00:38:19 +0800

    算法训练营 第二周

--- Week_02/TopKFrequentElements.java
+++ Week_02/TopKFrequentElements.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * ClassName: TopKFrequentElements
  * Description:
@@ -8,8 +13,29 @@
  */
 public class TopKFrequentElements {
 
-    public int[] topKFrequent(int[] nums, int k) {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
 
-        return null;
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+        return res;
     }
 }
