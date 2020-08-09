33fa6a8041f51fcaf375664b398adb76f9409197
YCKJ0837 <mingxiufeng@cloudwalk.cn>
2020-08-09 19:45:00 +0800

    算法训练营 第七周

---
+++ Week_07/ClimbingStairs.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: ClimbingStairs
+ * Description:
+ *
+ * @author mingxiufeng@cloudwalk.cn
+ * @version 1.5.3
+ * @date 2020/08/09
+ */
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        return -1;
+    }
+}
---
+++ Week_07/GenerateParentheses.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：n = 3
+ * 输出：[
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+public class GenerateParentheses {
+
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+    }
+}
