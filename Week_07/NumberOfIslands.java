17b492ff95f7cbd64e693036f93aa6472f15e5af
mingxiufeng <995142923@qq.com>
2020-08-11 08:17:31 +0800

    算法第八周

--- Week_07/NumberOfIslands.java
+++ Week_07/NumberOfIslands.java
@@ -39,6 +39,40 @@ public class NumberOfIslands {
      */
     public int numIslands(char[][] grid) {
 
-        return -1;
+        if(null==grid||grid.length==0||grid[0].length==0){
+            return 0;
+        }
+
+        int m=grid.length;
+        int n =grid[0].length;
+
+        int number = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='0'){
+                    continue;
+                }
+                dfs(grid,i,j);
+                number=number++;
+            }
+        }
+        return number;
+    }
+
+    /**
+     *
+     * @param grid
+     * @param i
+     * @param j
+     */
+    private void dfs(char[][] grid, int i, int j) {
+        if(i<0||j<0||i>=grid.length||j>=grid[0].length){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
 }
--- Week_07/SurroundedRegions.java
+++ Week_07/SurroundedRegions.java
@@ -28,6 +28,57 @@ public class SurroundedRegions {
      * @param board
      */
     public void solve(char[][] board) {
+        if(board==null||board.length==0){
+            return;
+        }
+        int m=board.length;
+        int n=board[0].length;
+        for(int i=0;i<n;i++){
+            //最上边的一行
+            if(board[0][i]=='O'){
+                dfs(0,i,board);
+            }
+            //最下边的一列
+            if(board[m-1][i]=='O'){
+                dfs(m-1,i,board);
+            }
+        }
+        for(int i=1;i<m-1;i++){
+            //最左边的一列
+            if(board[i][0]=='O'){
+                dfs(i,0,board);
+            }
+            //最右边的一列
+            if(board[i][n-1]=='O'){
+                dfs(i,n-1,board);
+            }
+        }
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param i
+     * @param j
+     * @param board
+     */
+    private void dfs(int i, int j, char[][] board) {
+        if(i<0||j<0||i>=board.length||j>=board[0].length|| board[i][j] != 'O'){
+            return;
+        }
+        board[i][j]='A';
+        dfs(i+1,j,board);
+        dfs(i-1,j,board);
+        dfs(i,j+1,board);
+        dfs(i,j-1,board);
     }
 }
